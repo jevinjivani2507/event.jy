22eec89008429fa6e37325269612e26ee0af1d3f
Jevin Jivani <jivani.jevin25@gmail.com>
2022-08-07 17:33:08 +0530

    EventList ADDED

---
+++ event.jy-fronted/src/components/EventCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const EventCard = () => {
+  return (
+    <div className="w-1/3 my-4 px-3">
+        <div className="h-80 rounded-2xl shadow-lg bg-green-300">
+            EventCard
+        </div>
+    </div>
+  )
+}
+
+export default EventCard;
--- event.jy-fronted/src/components/EventList.jsx
+++ event.jy-fronted/src/components/EventList.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import EventCard from './EventCard';
 
 const EventList = () => {
   return (
-    <div className="py-40">
-        <h1>Upcoming Events</h1>
+    <div className="mx-10 py-20 ">
+        <div className="flex justify-between">
+            <h1 className="font-bold text-2xl">Upcoming Events</h1>
+            <div className="flex gap-5">
+                <h1>Weekends</h1>
+                <h1>Event Type</h1>
+                <h1>Category</h1>
+            </div>
+        </div>
+        <div className="flex flex-wrap justify-between my-10">
+            <EventCard />
+            <EventCard />
+            <EventCard />
+            <EventCard />
+        </div>
     </div>
   )
 }
